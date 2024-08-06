e48c069c6d6f7bac0065abb71fa75635593f2955
gennaromazza <69857864+gennaromazza@users.noreply.github.com>
2024-08-06 18:32:26 +0200

    Update main_window.h

--- gui/main_window.h
+++ gui/main_window.h
@@ -2,15 +2,11 @@
 #define MAIN_WINDOW_H
 
 #include <QMainWindow>
-#include <QFileDialog>
-#include <QDate>
-#include "file_manager.h"
-#include "../image_converter.h"
-#include "create_job_dialog.h"
+#include "image_converter.h"  // Corretto percorso per image_converter.h
 
-namespace Ui {
-class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
@@ -21,13 +17,11 @@ public:
     ~MainWindow();
 
 private slots:
-    void compareDirectories();
-    void createJob();
+    void on_button_convert_clicked();
 
 private:
-    Ui::MainWindow *ui;
-    QString hardDisk1Path;
-    QString hardDisk2Path;
+    Ui::MainWindow *ui;  // Dichiarazione della classe Ui::MainWindow
+    ImageConverter imageConverter;
 };
 
 #endif // MAIN_WINDOW_H
