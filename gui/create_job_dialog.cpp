4993cc9ff1cb73f0ff5b9e10c7a55d012e687c81
gennaromazza <69857864+gennaromazza@users.noreply.github.com>
2024-08-06 18:57:59 +0200

    Update create_job_dialog.cpp

--- gui/create_job_dialog.cpp
+++ gui/create_job_dialog.cpp
@@ -1,9 +1,24 @@
 #include "create_job_dialog.h"
+#include "ui_create_job_dialog.h"
 
-CreateJobDialog::CreateJobDialog(QWidget *parent) : QDialog(parent) {
-    // Initialize UI elements
+CreateJobDialog::CreateJobDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CreateJobDialog)
+{
+    ui->setupUi(this);
 }
 
-CreateJobDialog::~CreateJobDialog() {
-    // Cleanup
+CreateJobDialog::~CreateJobDialog()
+{
+    delete ui;
+}
+
+QString CreateJobDialog::getJobName() const
+{
+    return ui->jobNameLineEdit->text();
+}
+
+QDate CreateJobDialog::getJobDate() const
+{
+    return ui->jobDateEdit->date();
 }
