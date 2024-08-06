f51b37283c433b087a4bf7829a41dd5dc1e70029
gennaromazza <69857864+gennaromazza@users.noreply.github.com>
2024-08-06 17:59:25 +0200

    Update main_window.h

--- gui/main_window.h
+++ gui/main_window.h
@@ -2,13 +2,27 @@
 #define MAIN_WINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QTextEdit>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
+    ~MainWindow();
+
+private slots:
+    void createJob();
+    void compareDisks();
+    void searchFiles();
+
+private:
+    QPushButton *createJobButton;
+    QPushButton *compareDisksButton;
+    QLineEdit *searchLineEdit;
+    QTextEdit *outputTextEdit;
 };
 
 #endif // MAIN_WINDOW_H
