ad011b4dcdbc4a8b73bd6221f5b9265eb68a1533
gennaromazza <69857864+gennaromazza@users.noreply.github.com>
2024-08-06 18:32:40 +0200

    Update main_window.cpp

--- gui/main_window.cpp
+++ gui/main_window.cpp
@@ -1,14 +1,11 @@
 #include "main_window.h"
 #include "ui_main_window.h"
-#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->compareButton, &QPushButton::clicked, this, &MainWindow::compareDirectories);
-    connect(ui->createJobButton, &QPushButton::clicked, this, &MainWindow::createJob);
 }
 
 MainWindow::~MainWindow()
@@ -16,35 +13,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::compareDirectories()
+void MainWindow::on_button_convert_clicked()
 {
-    hardDisk1Path = QFileDialog::getExistingDirectory(this, tr("Select Hard Disk 1"));
-    hardDisk2Path = QFileDialog::getExistingDirectory(this, tr("Select Hard Disk 2"));
-
-    if (hardDisk1Path.isEmpty() || hardDisk2Path.isEmpty()) {
-        QMessageBox::warning(this, tr("Warning"), tr("Please select both directories."));
-        return;
-    }
-
-    int count1 = FileManager::searchFiles(hardDisk1Path.toStdString(), "Full");
-    int count2 = FileManager::searchFiles(hardDisk2Path.toStdString(), "Full");
-
-    QMessageBox::information(this, tr("Search Results"),
-                             QString("Hard Disk 1: %1 file(s)\nHard Disk 2: %2 file(s)").arg(count1).arg(count2));
-}
-
-void MainWindow::createJob()
-{
-    CreateJobDialog dialog(this);
-    if (dialog.exec() == QDialog::Accepted) {
-        QString jobName = dialog.getJobName();
-        QDate jobDate = dialog.getJobDate();
-        QString directory = QFileDialog::getExistingDirectory(this, tr("Select Directory"));
-
-        if (!directory.isEmpty()) {
-            QString path = QString("%1/%2").arg(directory).arg(jobDate.toString("MM_dd_yyyy"));
-            FileManager::createDirectory(path.toStdString());
-            QMessageBox::information(this, tr("Success"), tr("Job created successfully."));
-        }
-    }
+    QString inputPath = ui->inputPathLineEdit->text();
+    QString outputPath = ui->outputPathLineEdit->text();
+    imageConverter.convert(inputPath.toStdString(), outputPath.toStdString());
 }
