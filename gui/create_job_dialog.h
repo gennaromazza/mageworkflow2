7ad0bfdd38de323c80d296434bb72c7676409db5
gennaromazza <69857864+gennaromazza@users.noreply.github.com>
2024-08-06 17:39:00 +0200

    Update create_job_dialog.h

--- gui/create_job_dialog.h
+++ gui/create_job_dialog.h
@@ -1,14 +1,9 @@
-#ifndef CREATE_JOB_DIALOG_H
-#define CREATE_JOB_DIALOG_H
+#include "create_job_dialog.h"
 
-#include <QDialog>
+CreateJobDialog::CreateJobDialog(QWidget *parent) : QDialog(parent) {
+    // Initialize UI elements
+}
 
-class CreateJobDialog : public QDialog {
-    Q_OBJECT
-
-public:
-    CreateJobDialog(QWidget *parent = nullptr);
-    ~CreateJobDialog() override; // Assicurarsi che il distruttore sia virtuale
-};
-
-#endif // CREATE_JOB_DIALOG_H
+CreateJobDialog::~CreateJobDialog() {
+    // Cleanup
+}
